62aef74024cc18b7ef93555d1e70d26aca7d08ad
Inkiluz <inkiluz@protonmail.com>
2020-01-30 14:22:27 +0000

    define 'O_TMPFILE'

--- include/uapi/asm-generic/fcntl.h
+++ include/uapi/asm-generic/fcntl.h
@@ -4,16 +4,16 @@
 #include <linux/types.h>
 
 /*
- * FMODE_EXEC is 0x20
- * FMODE_NONOTIFY is 0x1000000
- * These cannot be used by userspace O_* until internal and external open
- * flags are split.
- * -Eric Paris
- */
+ *  * FMODE_EXEC is 0x20
+ *   * FMODE_NONOTIFY is 0x1000000
+ *    * These cannot be used by userspace O_* until internal and external open
+ *     * flags are split.
+ *      * -Eric Paris
+ *       */
 
 /*
- * When introducing new O_* bits, please check its uniqueness in fcntl_init().
- */
+ *  * When introducing new O_* bits, please check its uniqueness in fcntl_init().
+ *   */
 
 #define O_ACCMODE	00000003
 #define O_RDONLY	00000000
@@ -63,18 +63,18 @@
 #endif
 
 /*
- * Before Linux 2.6.33 only O_DSYNC semantics were implemented, but using
- * the O_SYNC flag.  We continue to use the existing numerical value
- * for O_DSYNC semantics now, but using the correct symbolic name for it.
- * This new value is used to request true Posix O_SYNC semantics.  It is
- * defined in this strange way to make sure applications compiled against
- * new headers get at least O_DSYNC semantics on older kernels.
- *
- * This has the nice side-effect that we can simply test for O_DSYNC
- * wherever we do not care if O_DSYNC or O_SYNC is used.
- *
- * Note: __O_SYNC must never be used directly.
- */
+ *  * Before Linux 2.6.33 only O_DSYNC semantics were implemented, but using
+ *   * the O_SYNC flag.  We continue to use the existing numerical value
+ *    * for O_DSYNC semantics now, but using the correct symbolic name for it.
+ *     * This new value is used to request true Posix O_SYNC semantics.  It is
+ *      * defined in this strange way to make sure applications compiled against
+ *       * new headers get at least O_DSYNC semantics on older kernels.
+ *        *
+ *         * This has the nice side-effect that we can simply test for O_DSYNC
+ *          * wherever we do not care if O_DSYNC or O_SYNC is used.
+ *           *
+ *            * Note: __O_SYNC must never be used directly.
+ *             */
 #ifndef O_SYNC
 #define __O_SYNC	04000000
 #define O_SYNC		(__O_SYNC|O_DSYNC)
@@ -83,6 +83,14 @@
 #ifndef O_PATH
 #define O_PATH		010000000
 #endif
+
+#ifndef __O_TMPFILE
+#define __O_TMPFILE	020000000
+#endif
+
+/* a horrid kludge trying to make sure that this will fail on old kernels */
+#define O_TMPFILE (__O_TMPFILE | O_DIRECTORY)
+#define O_TMPFILE_MASK (__O_TMPFILE | O_DIRECTORY | O_CREAT)      
 
 #ifndef O_NDELAY
 #define O_NDELAY	O_NONBLOCK
@@ -124,13 +132,29 @@
 #define F_GETOWNER_UIDS	17
 #endif
 
+/*
+ *  * Open File Description Locks
+ *   *
+ *    * Usually record locks held by a process are released on *any* close and are
+ *     * not inherited across a fork().
+ *      *
+ *       * These cmd values will set locks that conflict with process-associated
+ *        * record  locks, but are "owned" by the open file description, not the
+ *         * process. This means that they are inherited across fork() like BSD (flock)
+ *          * locks, and they are only released automatically when the last reference to
+ *           * the the open file against which they were acquired is put.
+ *            */
+#define F_OFD_GETLK	36
+#define F_OFD_SETLK	37
+#define F_OFD_SETLKW	38
+
 #define F_OWNER_TID	0
 #define F_OWNER_PID	1
 #define F_OWNER_PGRP	2
 
 struct f_owner_ex {
-	int	type;
-	__kernel_pid_t	pid;
+		int	type;
+			__kernel_pid_t	pid;
 };
 
 /* for F_[GET|SET]FL */
@@ -153,7 +177,7 @@ struct f_owner_ex {
 #define LOCK_SH		1	/* shared lock */
 #define LOCK_EX		2	/* exclusive lock */
 #define LOCK_NB		4	/* or'd with one of the above to prevent
-				   blocking */
+				   				   blocking */
 #define LOCK_UN		8	/* remove lock */
 
 #define LOCK_MAND	32	/* This is a mandatory flock ... */
@@ -169,16 +193,14 @@ struct f_owner_ex {
 #endif
 
 struct flock {
-	short	l_type;
-	short	l_whence;
-	__kernel_off_t	l_start;
-	__kernel_off_t	l_len;
-	__kernel_pid_t	l_pid;
-	__ARCH_FLOCK_PAD
+		short	l_type;
+			short	l_whence;
+				__kernel_off_t	l_start;
+					__kernel_off_t	l_len;
+						__kernel_pid_t	l_pid;
+							__ARCH_FLOCK_PAD
 };
 #endif
-
-#ifndef CONFIG_64BIT
 
 #ifndef HAVE_ARCH_STRUCT_FLOCK64
 #ifndef __ARCH_FLOCK64_PAD
@@ -186,14 +208,13 @@ struct flock {
 #endif
 
 struct flock64 {
-	short  l_type;
-	short  l_whence;
-	__kernel_loff_t l_start;
-	__kernel_loff_t l_len;
-	__kernel_pid_t  l_pid;
-	__ARCH_FLOCK64_PAD
+		short  l_type;
+			short  l_whence;
+				__kernel_loff_t l_start;
+					__kernel_loff_t l_len;
+						__kernel_pid_t  l_pid;
+							__ARCH_FLOCK64_PAD
 };
 #endif
-#endif /* !CONFIG_64BIT */
 
 #endif /* _ASM_GENERIC_FCNTL_H */
